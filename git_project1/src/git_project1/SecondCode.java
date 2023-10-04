5eadc82f54c33fc893a9188f7555ebb4696c1ff2
root <root@ip-172-31-93-6.ec2.internal>
2023-10-04 14:19:45 +0000

    git_project1 committed.

---
+++ git_project1/src/git_project1/SecondCode.java
@@ -0,0 +1,6 @@
+package git_project1;
+
+public class test {
+        public static void main(String[] args) {
+             System.out.println("Hello, World! Second test code. Welcome to DevOps learning!!");
+
